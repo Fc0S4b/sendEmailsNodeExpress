8d6fcba0a5c4c134f7509ef55dcd1b8593dbdcf9
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-27 19:26:59 -0400

    send emails with ethereal

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,23 @@
+const nodemailer = require('nodemailer');
+require('dotenv');
 const sendEmail = async (req, res) => {
-  res.send('send email func');
+  let testAccount = await nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: process.env.ETHEREAL_USER,
+      pass: process.env.ETHEREAL_PASS,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Test user" <testuser@mail.com>',
+    to: 'bar@example.com',
+    subject: 'hello',
+    html: '<h2>sending emails with Node.js</h2>',
+  });
+  res.json(info);
 };
 
 module.exports = sendEmail;
